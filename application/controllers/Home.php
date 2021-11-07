600848d7830c6441a56430fab19fde5157f3ce12
amin vincent <amin@ignitech.net>
2021-11-07 22:55:30 +0700

    test dashbor

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		$this->load->library('session');
+		//$this->load->library('session');
 		$this->load->library('Layout');
 		$this->load->library('upload');
 		$this->load->model('masterModel');
